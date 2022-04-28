e2a39f00287225578bd5e523cde5d877917b44dc
MacBook Air M1 <macbook@sskate.me>
2022-04-29 00:15:05 +0900

    feat: add easter-egg

---
+++ js/123ive.js
@@ -0,0 +1,64 @@
+function _0x1bb7(_0xe68823, _0x41225d) {
+    const _0x2b6aa4 = _0x2b6a();
+    return (
+        (_0x1bb7 = function (_0x1bb775, _0x215e14) {
+            _0x1bb775 = _0x1bb775 - 0xb2;
+            let _0x5a4b89 = _0x2b6aa4[_0x1bb775];
+            return _0x5a4b89;
+        }),
+        _0x1bb7(_0xe68823, _0x41225d)
+    );
+}
+const _0x3fd47f = _0x1bb7;
+function _0x2b6a() {
+    const _0x32a9d7 = [
+        "귀여워버리기!\x20❤️",
+        "507946WrFqPG",
+        "3056790FiOpcS",
+        "length",
+        "744023HCtZBO",
+        "1720376UPRYHi",
+        "54vTHDqp",
+        "random",
+        "33SgoKMN",
+        "언니야들\x20사랑해요\x20❤️",
+        "floor",
+        "63sJurOr",
+        "img[src=\x22/img/logo.png\x22]",
+        "230444peyKDe",
+        "31735210CRgrWv",
+        "2147472ANJOYS",
+    ];
+    _0x2b6a = function () {
+        return _0x32a9d7;
+    };
+    return _0x2b6a();
+}
+(function (_0x58d5e1, _0x53ece1) {
+    const _0x54a852 = _0x1bb7,
+        _0x14d44c = _0x58d5e1();
+    while (!![]) {
+        try {
+            const _0x2b1e30 =
+                parseInt(_0x54a852(0xbb)) / 0x1 +
+                parseInt(_0x54a852(0xb9)) / 0x2 +
+                (parseInt(_0x54a852(0xb2)) / 0x3) * (-parseInt(_0x54a852(0xb7)) / 0x4) +
+                parseInt(_0x54a852(0xbc)) / 0x5 +
+                (parseInt(_0x54a852(0xc0)) / 0x6) * (parseInt(_0x54a852(0xbe)) / 0x7) +
+                (parseInt(_0x54a852(0xbf)) / 0x8) * (parseInt(_0x54a852(0xb5)) / 0x9) +
+                -parseInt(_0x54a852(0xb8)) / 0xa;
+            if (_0x2b1e30 === _0x53ece1) break;
+            else _0x14d44c["push"](_0x14d44c["shift"]());
+        } catch (_0x1bbe1a) {
+            _0x14d44c["push"](_0x14d44c["shift"]());
+        }
+    }
+})(_0x2b6a, 0xcef70),
+    document["querySelector"](_0x3fd47f(0xb6))["addEventListener"]("click", function () {
+        const _0x174c75 = _0x3fd47f;
+        if (Math[_0x174c75(0xb4)](Math[_0x174c75(0xc1)]() * 0x5) + 0x1 == 0x1) {
+            let _0x4dc08f = [_0x174c75(0xb3), _0x174c75(0xba), "중간고사\x20있어요...\x20후에엥...\x20😭"];
+            alert(_0x4dc08f[Math["floor"](Math[_0x174c75(0xc1)]() * _0x4dc08f[_0x174c75(0xbd)])]);
+        }
+    });
+
