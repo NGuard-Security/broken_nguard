508d428c9de6c276d8f59e7878331f9415f907be
MacBook Air M1 <macbook@sskate.me>
2022-04-30 15:17:28 +0900

    fix: 0일 0시간 0분 0초 해결

--- assets/js/modules/countdown.js
+++ assets/js/modules/countdown.js
@@ -1,5 +1,5 @@
 const countDownTimer = (selector, date) => {
-    var _vDate = new Date(date); // 전달 받은 일자
+    var _vDate = new Date(date);
     var _second = 1000;
     var _minute = _second * 60;
     var _hour = _minute * 60;
@@ -21,10 +21,28 @@ const countDownTimer = (selector, date) => {
         var minutes = Math.floor((distDt % _hour) / _minute);
         var seconds = Math.floor((distDt % _minute) / _second);
 
-        document.querySelector(selector).querySelector('span:nth-child(1)').textContent = days;
-        document.querySelector(selector).querySelector('span:nth-child(2)').textContent = hours;
-        document.querySelector(selector).querySelector('span:nth-child(3)').textContent = minutes;
-        document.querySelector(selector).querySelector('span:nth-child(4)').textContent = seconds;
+        if (days <= 0) {
+            document.querySelector(selector).querySelector('span:nth-child(1)').style = "display: none;"
+        } else {
+            document.querySelector(selector).querySelector('span:nth-child(1)').style = ""
+        }
+
+        if (hours <= 0) {
+            document.querySelector(selector).querySelector('span:nth-child(2)').style = "display: none;"
+        } else {
+            document.querySelector(selector).querySelector('span:nth-child(2)').style = ""
+        }
+
+        if (minutes <= 0) {
+            document.querySelector(selector).querySelector('span:nth-child(3)').style = "display: none;"
+        } else {
+            document.querySelector(selector).querySelector('span:nth-child(3)').style = ""
+        }
+
+        document.querySelector(selector).querySelector('span:nth-child(1)').textContent = days + "일";
+        document.querySelector(selector).querySelector('span:nth-child(2)').textContent = hours + "시간";
+        document.querySelector(selector).querySelector('span:nth-child(3)').textContent = minutes + "분";
+        document.querySelector(selector).querySelector('span:nth-child(4)').textContent = seconds + "초";
     }
 
     showRemaining()
