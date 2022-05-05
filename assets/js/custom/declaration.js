1248ee9cd3ddbd793d1a08f9ef94931a66ac6c40
MacBook Air M1 <macbook@sskate.me>
2022-05-05 19:42:39 +0900

    feat: prettier

--- assets/js/custom/declaration.js
+++ assets/js/custom/declaration.js
@@ -1,4 +1,4 @@
-document.querySelector('.logo').addEventListener('click', () => {
+document.querySelector(".logo").addEventListener("click", () => {
     let array = [
         "꽃은 저마다 필 시기가 있다.",
         "버티는게 이기는 거다.",
@@ -10,11 +10,11 @@ document.querySelector('.logo').addEventListener('click', () => {
         "언제나 현재에 집중할 수 있다면 행복할 것이다.",
         "좋은 성과를 얻으려면 한 걸음 한 걸음이 힘차고 충실하지 않으면 안된다.",
         "갈을 잃는다는 것은 곧 길을 알게 된다는 것이다.",
-        "오늘은 어제와 다르고, 내일은 오늘과 다를 것이다.\n\n어제 열심히 살았으면, 오늘은 밝아져있고,\n\n오늘을 열심히 살면, 내일이 밝아진다."
-    ]
+        "오늘은 어제와 다르고, 내일은 오늘과 다를 것이다.\n\n어제 열심히 살았으면, 오늘은 밝아져있고,\n\n오늘을 열심히 살면, 내일이 밝아진다.",
+    ];
 
     let random = Math.floor(Math.random() * (array.length - 3)) + 1;
     if (random == array.length - 5) {
-        alert(array[Math.floor(Math.random() * array.length)])
+        alert(array[Math.floor(Math.random() * array.length)]);
     }
-})
+});
--- assets/js/modules/countdown.js
+++ assets/js/modules/countdown.js
@@ -12,7 +12,7 @@ const countDownTimer = (selector, date) => {
 
         if (distDt < 0) {
             clearInterval(timer);
-            document.querySelector(selector).textContent = '카운트 다운이 종료 되었습니다!';
+            document.querySelector(selector).textContent = "카운트 다운이 종료 되었습니다!";
             return;
         }
 
@@ -22,30 +22,30 @@ const countDownTimer = (selector, date) => {
         var seconds = Math.floor((distDt % _minute) / _second);
 
         if (days <= 0) {
-            document.querySelector(selector).querySelector('span:nth-child(1)').style = "display: none;"
+            document.querySelector(selector).querySelector("span:nth-child(1)").style = "display: none;";
         } else {
-            document.querySelector(selector).querySelector('span:nth-child(1)').style = ""
+            document.querySelector(selector).querySelector("span:nth-child(1)").style = "";
         }
 
         if (hours <= 0) {
-            document.querySelector(selector).querySelector('span:nth-child(2)').style = "display: none;"
+            document.querySelector(selector).querySelector("span:nth-child(2)").style = "display: none;";
         } else {
-            document.querySelector(selector).querySelector('span:nth-child(2)').style = ""
+            document.querySelector(selector).querySelector("span:nth-child(2)").style = "";
         }
 
         if (minutes <= 0) {
-            document.querySelector(selector).querySelector('span:nth-child(3)').style = "display: none;"
+            document.querySelector(selector).querySelector("span:nth-child(3)").style = "display: none;";
         } else {
-            document.querySelector(selector).querySelector('span:nth-child(3)').style = ""
+            document.querySelector(selector).querySelector("span:nth-child(3)").style = "";
         }
 
-        document.querySelector(selector).querySelector('span:nth-child(1)').textContent = days + "일";
-        document.querySelector(selector).querySelector('span:nth-child(2)').textContent = hours + "시간";
-        document.querySelector(selector).querySelector('span:nth-child(3)').textContent = minutes + "분";
-        document.querySelector(selector).querySelector('span:nth-child(4)').textContent = seconds + "초";
+        document.querySelector(selector).querySelector("span:nth-child(1)").textContent = days + "일";
+        document.querySelector(selector).querySelector("span:nth-child(2)").textContent = hours + "시간";
+        document.querySelector(selector).querySelector("span:nth-child(3)").textContent = minutes + "분";
+        document.querySelector(selector).querySelector("span:nth-child(4)").textContent = seconds + "초";
     }
 
-    showRemaining()
-    
+    showRemaining();
+
     timer = setInterval(showRemaining, 1000);
-}
+};
