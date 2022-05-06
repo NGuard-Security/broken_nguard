0b5446b27ada637a359d443a0ef9d7fe67294efe
MacBook Air M1 <macbook@sskate.me>
2022-05-06 16:42:19 +0900

    feat: typo & code formatting

--- assets/js/common/contact.js
+++ assets/js/common/contact.js
@@ -1,5 +1,5 @@
 ((a) => {
-    a.querySelector('.contact').innerHTML = `
+  a.querySelector(".contact").innerHTML = `
         문의 : 
         <a href="javascript:window.ChannelIO('showMessenger');" rel="noopener noreferrer">하단 채널톡</a>, 
         <a href="https://discord.gg/kEcKQCScaD" target="_blank" rel="noopener noreferrer">디스코드 서포트 서버</a>
