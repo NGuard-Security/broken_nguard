7c7a0c8902740289544a4fc1fe5867c708ff44f3
SSKATE <kms0219kms@naver.com>
2022-05-13 07:16:18 +0900

    Update contact.js

--- assets/js/common/contact.js
+++ assets/js/common/contact.js
@@ -2,6 +2,6 @@
   a.querySelector(".contact").innerHTML = `
         문의 : 
         <a href="javascript:window.ChannelIO('showMessenger');" rel="noopener noreferrer">하단 채널톡</a>, 
-        <a href="/invite/nguard" target="_blank" rel="noopener noreferrer">디스코드 서포트 서버</a>
+        <a href="https://nguard.xyz/invite/nguard" target="_top" rel="noopener noreferrer">디스코드 서포트 서버</a>
     `;
 })(document);
