a5303e0ae4641d2066daaf6859e7044b759573f2
MacBook Air M1 <macbook@sskate.me>
2022-05-19 18:31:11 +0900

    feat: website renewal

--- assets/js/common/contact.js
+++
@@ -1,7 +0,0 @@
-((a) => {
-  a.querySelector(".contact").innerHTML = `
-        문의 : 
-        <a href="javascript:window.ChannelIO('showMessenger');" rel="noopener noreferrer">하단 채널톡</a>, 
-        <a href="https://nguard.xyz/invite/nguard" target="_top" rel="noopener noreferrer">디스코드 서포트 서버</a>
-    `;
-})(document);
